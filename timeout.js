336c1e0a287db2dfd89bbf397e256b4292fd380b
Rabea Basri <rbasri556@gmail.com>
2021-02-06 10:11:23 -0800

    this is my api-try

---
+++ timeout.js
@@ -0,0 +1,18 @@
+function doSomething(){
+    console.log(3333);
+}
+// console.log(2222);
+// setTimeout(function(){
+//     console.log('lazy and waiting')
+
+// },4000)
+// setTimeout(() =>{
+//     console.log('see you later.')
+
+// },4000)
+setInterval(function(){
+    console.log('doing it again')
+},3000)
+console.log(44444);
+console.log(44444);
+console.log(44444);
